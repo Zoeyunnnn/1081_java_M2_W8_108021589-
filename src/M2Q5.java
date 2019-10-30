ae215ebf45f729794e039a14a1ad80767ada76b9
Zoeyunnnn <hicandy0406@gmail.com>
2019-10-30 15:57:06 +0800

    First Commit

--- src/M2Q5.java
+++ src/M2Q5.java
@@ -6,5 +6,14 @@ public class M2Q5 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+        String str="";
+        while (n>0) {
+            str = Integer.toString((n%2))+str;
+            n /= 2;
+        }
+        /*int a = str.length();
+        for (int i=1;i<=8-a;i++){
+            str = "0"+str;}*/
+        System.out.printf("%08d\n",Integer.parseInt(str));
     }
 }
